f3cea329c12226f76ef6b024351ce96f54231622
juan <juan.shelton17@gmail.com>
2024-12-17 10:55:59 -0300

    first commit

---
+++ src/test/java/com/transacaoautorizador/TransacaoAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transacaoautorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoAutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
